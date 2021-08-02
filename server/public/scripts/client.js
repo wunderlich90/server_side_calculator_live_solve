071891b49b279c1a743e658cb2612ac1c6dec996
Jeremy Cady <jcady90@icloud.com>
2021-08-02 12:12:45 -0500

    clear

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,96 @@
 $(document).ready(onReady);
 
+let operator = '';
+
 function onReady() {
     console.log('So ready!!');
     
-   
+   // click listeners
+   $('#submit').on('click', readySubmit);
+   $('.operator').on('click', readyOperatorClick);
+   // LOAD then GET
+   getAllCalculations();
 
+}
+
+function readyOperatorClick() {
+    console.log('clicked operand');
+    operator = ($(this).text());
+    
+}
+
+function renderCalculations(calculationsArray) {
+    // append to DOM
+    // first clear DOM since it's out of date. NO DUPES!!
+    $('#allCalcs').empty();
+
+    // show last calc result
+    $('#lastCalc').text(calculationsArray[calculationsArray.length-1].result);
+
+    // loop to append the DOM
+    for(let calc of calculations) {
+        $('#allCalcs').append(`<li>
+            ${calc.num1}
+            ${calc.operator}
+            ${calc.num2}
+            ${calc.result}
+         `)
+    }
+}
+
+function getAllCalculations() {
+    // a promise -- asynchronous code -- this means it takes time
+    $.ajax({
+        method: 'GET',
+        url: '/calculation',
+    }).then((response) => {
+        //for sure server has responded
+        console.log(response);
+        renderCalculations(response);
+        
+    })
+}
+
+function readySubmit() {
+    console.log('clicked');
+    const num1 = $('#num1').val();
+    const num2 = $('#num2').val();
+
+    //console.log(num1, num2, operator);
+
+    // go to server with our data
+    // package our data
+    // objects are the best way to send data to server
+    const dataToSend = {
+        num1: $('#num1').val(),
+        num2: $('#num2').val(),
+        operator: operator
+    }
+    console.log('dataToSend');
+    
+    // send to server http request
+    // this pattern is called a promise:
+    $.ajax({
+        method: 'POST',
+        url: '/calculation',
+        data: dataToSend
+    }).then(function(response){
+        // for sure server has responded
+        console.log(response);
+        // what to do??
+        // GET ALL CALCULATIONS
+        getAllCalculations();
+
+        
+    })
+
+    // server probably not yet done
+
+
+
+
+    //....wait
+    // whe it comes back do something...? GET?
+    
 }
 
--- server/server.js
+++ server/server.js
@@ -9,19 +9,72 @@ const bodyParser = require('body-parser');
 // create our "app" (server)
 const app = express();
 
-// Data
-
-
-
-// Tell express where to find all of our "public" files (aka "client side" files)
-// We also call these "static assets"
-app.use(express.static('./server/public'));
-
 // Setup body parser
 // Tells express how to read data from the client
 // either as JSON, or url-encoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Tell express where to find all of our "public" files (aka "client side" files)
+// We also call these "static assets"
+app.use(express.static('./server/public'));
+
+// data storage
+let calculationsArray = [];
+
+// ROUTES
+app.get('calculation', (req, res) => {
+    // send back to the client all calculations
+    res.send(calculationsArray);
+})
+
+app.post('/calculation', (req, res) => {
+    // where's our data
+    console.log(calculationsArray);
+    
+    console.log('here is our data', req.body);
+
+    // do calculations
+    //if(Request.BODY.OPERATOR === )
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let operator = req.body.operator;
+    let result = 0;
+    
+    switch(req.body.operator) {
+        case '+':
+            //do add
+            result = num1 + Number(req.body.num2)
+            break;
+        case '-':
+            //do subtract
+            result = num1 - Number(req.body.num2)
+            break;
+        case '*':
+            //do multiply
+            result = num1 * Number(req.body.num2)
+            break;
+        case '/':
+            //do division
+            result = num1 / Number(req.body.num2)
+            break;
+    }
+    console.log(result);
+    
+
+    // save in array;
+    const completedCalc = {
+        num1: num1,
+        num2: num2,
+        operator: operator,
+        result: result
+    }
+    calculationsArray.push(completedCalc);
+    
+    // all good routes respond with something
+    res.sendStatus(200);  // CREATED
+})
+
 
 
 
